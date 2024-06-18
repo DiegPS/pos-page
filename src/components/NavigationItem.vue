212f0730a8c959d154836d59f46af81c06a19c97
Diego Pahua Silvan <dpahuas@gmail.com>
2024-06-18 11:15:07 -0600

    feat(chore): Add navigation component and update router configuration

---
+++ src/components/NavigationItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/tip-payments">Tip Payments</RouterLink>
+    <RouterLink to="/settings">Settings</RouterLink>
+    <RouterLink to="/account">Account</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+  </nav>
+</template>
