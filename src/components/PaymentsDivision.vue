c3977ca8807679791cb62ceeef3b54ada29ff4b3
Diego Pahua Silvan <dpahuas@gmail.com>
2024-06-18 20:13:51 -0600

    feat: Remove PayMethods component and update PaymentsDivision with new payment options

--- src/components/PaymentsDivision.vue
+++ src/components/PaymentsDivision.vue
@@ -2,6 +2,8 @@
 import AmountItem from '@/components/AmountItem.vue'
 import IconPencil from './icons/IconPencil.vue'
 import IconWallet from './icons/IconWallet.vue'
+import IconCash from './icons/IconCash.vue'
+import IconCreditCard from './icons/IconCreditCard.vue'
 defineProps<{
   amount: string[]
 }>()
@@ -23,9 +25,31 @@ defineProps<{
       <IconWallet />
       <h3>Elige el Método de Pago</h3>
     </footer>
+    <custom-element>
+      <span><IconCash />Efectivo</span>
+      <span><IconCreditCard />Santander 2323</span>
+      <span><IconCreditCard /> BBVA 3213</span>
+    </custom-element>
   </section>
 </template>
 <style scoped>
+custom-element {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr 1fr;
+}
+span {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 3rem;
+  justify-content: center;
+  border-radius: 8px;
+  box-shadow: 0 0 5px #3d3d3d;
+  font-weight: 700;
+  color: #3d3d3d;
+  padding: 1rem;
+}
 section {
   display: flex;
   flex-direction: column;
