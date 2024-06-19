dceeb9183787cad967bb9c65fae523dc3d9d5624
Diego Pahua Silvan <dpahuas@gmail.com>
2024-06-18 21:35:39 -0600

    feat: Add TotalTips component for displaying total tips amount

--- src/components/PaymentsDivision.vue
+++ src/components/PaymentsDivision.vue
@@ -1,23 +1,16 @@
 <script setup lang="ts">
-import AmountItem from '@/components/AmountItem.vue'
-import IconPencil from './icons/IconPencil.vue'
 import IconWallet from './icons/IconWallet.vue'
 import IconCash from './icons/IconCash.vue'
 import IconCreditCard from './icons/IconCreditCard.vue'
+import TotalTips from './TotalTips.vue'
 defineProps<{
   amount: string[]
 }>()
 </script>
 <template>
   <section>
+    <TotalTips :amount="amount" />
     <header>
-      <article>
-        <p>Total de Propinas</p>
-        <div>
-          <AmountItem :amount="amount.join('')" />
-          <IconPencil />
-        </div>
-      </article>
       <h3>¿Entre cúantos quieres dividir las Propinas?</h3>
       <input />
     </header>
@@ -25,15 +18,15 @@ defineProps<{
       <IconWallet />
       <h3>Elige el Método de Pago</h3>
     </footer>
-    <custom-element>
+    <article>
       <span><IconCash />Efectivo</span>
       <span><IconCreditCard />Santander 2323</span>
       <span><IconCreditCard /> BBVA 3213</span>
-    </custom-element>
+    </article>
   </section>
 </template>
 <style scoped>
-custom-element {
+article {
   display: grid;
   gap: 1rem;
   grid-template-columns: 1fr 1fr;
@@ -64,25 +57,13 @@ footer {
   align-items: center;
   gap: 0.5rem;
 }
-div {
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-}
+
 svg {
   color: #3d3d3d;
 }
-article {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+
 h3 {
   color: #3d3d3d;
-  font-weight: 700;
-}
-p {
-  color: #ed6959;
   font-weight: 700;
 }
 </style>
