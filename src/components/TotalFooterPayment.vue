98a32da7f9be9c715b583e9093c83db527e59d07
Diego Pahua Silvan <dpahuas@gmail.com>
2024-06-18 19:48:48 -0600

    feat: Add TotalFooterPayment and IconCreditCard components

---
+++ src/components/TotalFooterPayment.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <aside>
+      <header>
+        <p>Total pagado</p>
+        <p>amount</p>
+      </header>
+      <footer>
+        <p>Restante por pagar</p>
+        <p>amount</p>
+      </footer>
+    </aside>
+    <button>Pagar Propinas</button>
+  </section>
+</template>
+<style scoped>
+p {
+  font-size: 1.8rem;
+  font-weight: 700;
+}
+aside {
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  width: 40rem;
+}
+header {
+  color: #ed6959;
+  display: flex;
+  justify-content: space-between;
+}
+footer {
+  display: flex;
+  justify-content: space-between;
+  color: #3d3d3d;
+}
+button {
+  border: 3px solid #c1c1c1;
+  border-radius: 30px;
+  padding: 0.8rem 3rem;
+  font-size: 2rem;
+  font-weight: 700;
+  color: #c1c1c1;
+}
+section {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 1rem 2rem;
+}
+</style>
